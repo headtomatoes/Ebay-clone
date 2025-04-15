a794dd9d3774a5462771641f387c1122311331b9
AnhNe <trongle250504@gmail.com>
2025-04-16 02:48:52 +0700

    add constraint for SignupRequestDTO

--- backend/src/main/java/com/gambler99/ebay_clone/dto/SignupRequestDTO.java
+++ backend/src/main/java/com/gambler99/ebay_clone/dto/SignupRequestDTO.java
@@ -1,6 +1,8 @@
 package com.gambler99.ebay_clone.dto;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -18,8 +20,15 @@ public class SignupRequestDTO {
     private String username;
 
     @NotBlank(message = "Email is required")
+    @Email(message = "Please provide a valid email address")
     private String email;
 
     @NotBlank(message = "Password is required")
+    @Size(min = 8, max = 20, message = "Password must be between 8 and 20 characters long")
+    @Pattern(
+            regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?=\\S+$).*$",
+            message = "Password must contain at least one digit, one lowercase letter, " +
+                    "one uppercase letter, one special character, and no whitespace"
+        )
     private String password;
 }
