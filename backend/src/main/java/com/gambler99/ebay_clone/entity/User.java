9bf7755fb1b340ef8f527cf87b8939c0cc8a02bf
AnhNe <trongle250504@gmail.com>
2025-04-18 20:31:23 +0700

    fix bug in User.java

--- backend/src/main/java/com/gambler99/ebay_clone/entity/User.java
+++ backend/src/main/java/com/gambler99/ebay_clone/entity/User.java
@@ -11,16 +11,13 @@ import java.util.Set;
 
 @Entity
 @Table(name = "users" ,
-        //tl;dr: indexes are used to speed up the WHERE and JOIN clauses
+        //tl;dr: indexes are used to speed up the WHERE and JOIN clauses,
         //and these columns are likely to be frequently used search criteria
         indexes = {
             @Index(name = "idx_email" , columnList = "email"),
             @Index(name = "idx_google_id" , columnList = "google_id"),
         })
-@Getter
-@Setter
-@ToString
-@RequiredArgsConstructor
+@Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
@@ -75,19 +72,4 @@ public class User {
     )
     private Set<Role> roles = new HashSet<>();
 
-    @Override
-    public final boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null) return false;
-        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
-        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
-        if (thisEffectiveClass != oEffectiveClass) return false;
-        User user = (User) o;
-        return getUserId() != null && Objects.equals(getUserId(), user.getUserId());
-    }
-
-    @Override
-    public final int hashCode() {
-        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
-    }
 }
