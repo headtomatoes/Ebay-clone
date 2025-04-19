868845338334088ef1e353703f5caf487479c52e
LAPTOP <lenhatanh208@gmail.com>
2025-04-20 02:04:24 +0700

    entity +repo categori + product

---
+++ backend/src/main/java/com/gambler99/ebay_clone/entity/categories.java
@@ -0,0 +1,42 @@
+package com.gambler99.ebay_clone.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+
+import java.time.LocalDateTime;
+
+
+
+
+@Entity
+@Table (name = "categories")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class categories {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id", nullable = false)
+    private Long categoryId;
+
+    @Column(name = "name", nullable = false, length = 100)
+    private String name;
+
+    @Column(name = "description", columnDefinition = "text")
+    private String description;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_category_id", referencedColumnName = "category_id", foreignKey = @ForeignKey(name = "FK_PARENT_CATEGORY"))
+    private categories parentCategory;
+
+
+}
