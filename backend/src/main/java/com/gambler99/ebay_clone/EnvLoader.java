2e3f4f7387f085972d9aa2605ae56854df52617d
Le <ITCSIU22271@student.hcmiu.edu.vn>
2025-05-22 10:50:27 +0700

    add create auction diagram

--- backend/src/main/java/com/gambler99/ebay_clone/EnvLoader.java
+++ backend/src/main/java/com/gambler99/ebay_clone/EnvLoader.java
@@ -7,6 +7,11 @@ public class EnvLoader {
        System.setProperty("DB_URL", dotenv.get("DB_URL"));
        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+       System.setProperty("GOOGLE_CLIENT_ID", dotenv.get("GOOGLE_CLIENT_ID"));
+       System.setProperty("GOOGLE_CLIENT_SECRET", dotenv.get("GOOGLE_CLIENT_SECRET"));
+
+       System.setProperty("EMAIL_USER", dotenv.get("EMAIL_USER"));
+       System.setProperty("EMAIL_PASS", dotenv.get("EMAIL_PASS"));
    }
 
 }
